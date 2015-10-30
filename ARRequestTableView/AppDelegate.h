e42dab28274f98bd1100e0040eed2660780aa03b
Andres Rizzo <andresrizzo@MacBook-Pro-de-Andres.local>
2015-10-30 16:37:00 -0300

    Initial commit

---
+++ ARRequestTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARRequestTableView
+//
+//  Created by Andres Rizzo on 30/10/15.
+//  Copyright © 2015 ANDzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ARRequestTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARRequestTableView
+//
+//  Created by Andres Rizzo on 30/10/15.
+//  Copyright © 2015 ANDzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
